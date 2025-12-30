bcaccaf2ab6b20e0982fd4810ce6c7ca92e5e82b
0xdvck <0xdvck@gmail.com>
2025-12-31 00:45:52 +0700

    update vite config for portforwarding

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000 // Change to your desired port
+    port: 8000, // Change to your desired port
+    host: '0.0.0.0'
   }
 })
